8020a3bacafa6a70cd9940a85b7bc3a80412218c
ITR <ITR@live.no>
2020-01-27 20:53:36 +0100

    Removed debug function

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 function OnPlayerSpawned( player_entity )
 	local x, y = EntityGetTransform( player_entity )
-	EntityLoad("data/entities/_debug/spawn_perk.xml", x, y )
+	-- EntityLoad("data/entities/_debug/spawn_perk.xml", x, y )
 end
 
 FILE_ROOT = "mods/perks_by_itr/files/"
