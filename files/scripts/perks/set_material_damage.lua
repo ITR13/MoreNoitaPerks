442787cb29ef9db955016981a031d0293b809bad
ITR <ITR@live.no>
2020-01-27 19:25:19 +0100

    Added methods to modify material damage on an entity

---
+++ files/scripts/perks/set_material_damage.lua
@@ -0,0 +1,93 @@
+dofile_once( "data/scripts/lib/utilities.lua" )
+
+function zip(...)
+  local arrays, ans = {...}, {}
+  local index = 0
+  return
+    function()
+      index = index + 1
+      for i,t in ipairs(arrays) do
+        if type(t) == 'function' then ans[i] = t() else ans[i] = t[index] end
+        if ans[i] == nil then return end
+      end
+      return unpack(ans)
+    end
+end
+
+
+function set_material_damage(entity, rebind_table)
+	edit_component( entity, "DamageModelComponent", function(comp,vars)
+		names = ComponentGetValue( comp, "materials_that_damage", "")
+		values = ComponentGetValue( comp, "materials_how_much_damage", "" )
+
+		new_values = ""
+
+		for name, value in zip(string.gmatch(names, '([^,]+)'), string.gmatch(values, '([^,]+)')) do
+			if rebind_table[name] ~= nil then
+				value = tostring(rebind_table[name])
+				rebind_table[name] = nil
+			end
+			if new_values then
+				new_values = new_values..","..value
+			else
+				new_values = value
+			end
+		end
+
+		for name, value in pairs(rebind_table) do
+			if new_values then
+				new_values = new_values..","..value
+			else
+				new_values = value
+			end
+			if names then
+				names = names..","..name
+			else
+				names = name
+			end
+		end
+		ComponentSetValue( comp, "materials_that_damage", names )
+		ComponentSetValue( comp, "materials_how_much_damage", new_values )
+	end)
+	local polymorph = GetGameEffectLoadTo( entity, "POLYMORPH", true );
+	ComponentSetValue( polymorph, "frames", 1 );
+end
+
+function get_material_damage(entity, material_name)
+	found_value = nil
+	edit_component( entity, "DamageModelComponent", function(comp,vars)
+		names = ComponentGetValue( comp, "materials_that_damage", "")
+		values = ComponentGetValue( comp, "materials_how_much_damage", "" )
+		for name, value in zip(string.gmatch(names, '([^,]+)'), string.gmatch(values, '([^,]+)')) do
+			if name == material_name then
+				found_value = value
+				break
+			end
+		end
+	end)
+	return found_value
+end
+
+function multiply_material_damage(entity, factor)
+	edit_component( entity, "DamageModelComponent", function(comp,vars)
+		names = ComponentGetValue( comp, "materials_that_damage", "")
+		values = ComponentGetValue( comp, "materials_how_much_damage", "" )
+
+		new_value = tostring(new_value)
+		new_values = ""
+		material_found = false
+
+		for name, value in zip(string.gmatch(names, '([^,]+)'), string.gmatch(values, '([^,]+)')) do
+			value = tostring(tonumber(value) * factor)
+			if new_values then
+				new_values = new_values..","..value
+			else
+				new_values = value
+			end
+		end
+
+		ComponentSetValue( comp, "materials_how_much_damage", new_values )
+	end)
+	local polymorph = GetGameEffectLoadTo( entity, "POLYMORPH", true );
+	ComponentSetValue( polymorph, "frames", 1 );
+end
